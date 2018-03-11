dabaa555c630eadd9ff8f067670c68c82e8ac7ec
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-03-11 00:03:18 -0500

    cleaned up both ApartmentsContainer and ApartmentShowContainer files

--- app/javascript/react/containers/ApartmentShowContainer.js
+++ app/javascript/react/containers/ApartmentShowContainer.js
@@ -12,8 +12,7 @@ class ApartmentShowContainer extends Component {
   }
   componentDidMount(){
     let apartmentId = this.props.params.id
-    // console.log(this.props.params.id)
-    // let apartmentId = document.getElementById('show').getAttribute('data-id');
+
     fetch(`/api/v1/apartments/${apartmentId}`)
       .then(response => {
         if (response.ok) {
@@ -25,21 +24,6 @@ class ApartmentShowContainer extends Component {
         }
       })
 
-      // .then(response => response.json())
-      // .then(json => {
-      //   debugger;
-      // })
-
-    // fetch('api/v1/apartments/1')
-    //   .then(response => {
-    //     if (response.ok) {
-    //       return response;
-    //     } else {
-    //       let errorMessage = `${response.status}(${response.statusText})`,
-    //         error = new Error(errorMessage);
-    //       throw(error);
-    //     }
-    //   })
       .then(response => response.json())
       .then(body => {
         this.setState({ apartmentInfo: body.apartment });
@@ -47,36 +31,10 @@ class ApartmentShowContainer extends Component {
       .catch(error => console.error(`Error in fetch: ${error.message}`));
 
   }
-    // .then(response => {
-//     let parsed = response.json()
-//     .then(json => {
-//       debugger;
-//     })
-//     }
-//
-//   .then(cereal => {
-//     this.setState({ apartmentInfo: cereal})
-//   })
-// }
 
   render(){
-
-      // console.log(JSON.parse('this.props.params.id'))
-      // let apartmentShowObjects = this.state.apartmentInfo.map((apt) =>{
-      //   return(
-      //     <ApartmentInformation
-      //       id={apt.id}
-      //       description={apt.description}
-      //       price={apt.price}
-      //       />
-      //   )
-    // }
-
     return(
-      // <div>
-      //   <h1>Mira esta página increíble</h1>
-      //   {apartmentShowObjects}
-      //   </div>
+  
       <ApartmentInformation
         id={this.state.apartmentInfo.id}
         title={this.state.apartmentInfo.title}
