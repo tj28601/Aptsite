5084aac163cf8c39e8f9df5f09f32098b81b7f61
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-04-06 18:15:52 -0400

    adding more RSPEC tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'factory_bot'
-
-FactoryBot.define do
-  factory :user do
-    sequence(:user_name) { |n| "user#{n}"}
-    sequence(:email) {|n| "user#{n}@example.com" }
-    password 'password'
-    password_confirmation 'password'
-    role 'member'
-  end
-end
-henryk = FactoryBot.create(:user, role: "admin")
-jascha = FactoryBot.create(:user)
-mstislav = FactoryBot.create(:user)
-user123 = FactoryBot.create(user_name: "bob", role: "admin", email: "clown@gmail.com", password: "password")
+# require 'factory_bot'
+#
+# FactoryBot.define do
+#   factory :user do
+#     sequence(:user_name) { |n| "user#{n}"}
+#     sequence(:email) {|n| "user#{n}@example.com" }
+#     password 'password'
+#     password_confirmation 'password'
+#     role 'member'
+#   end
+# end
+# henryk = FactoryBot.create(:user, role: "admin")
+# jascha = FactoryBot.create(:user)
+# mstislav = FactoryBot.create(:user)
+# user123 = FactoryBot.create(user_name: "bob", role: "admin", email: "clown@gmail.com", password: "password")
 
 apt1 = Apartment.find_or_create_by(title: "Nice 3BR 1.5 BA in Great Location!", description: "this apartment is located in fantastic, family-oriented neighborhood. NO DOGS ALLOWED", price: "$800", address: "47 8th Ave NE Hickory, NC 28602", bedrooms: "3", bathrooms: "1.5", sq_ft: "900", pets: "No Dogs allowed, but cats are okay. Meow!")
 apt2 = Apartment.find_or_create_by(title: "2 BR 3BA Apt in Historic Downtown", description: "this apt is close to various bars and restaurants. Amazing location!", price: "$750", address: "24 2nd Ave SW Hickory, NC 28601", bedrooms: "2", bathrooms: "3", sq_ft: "750", pets: "No Pets.")
