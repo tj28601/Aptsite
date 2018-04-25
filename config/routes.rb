af61e626795953da510e3ae48120c3114836dac3
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-04-25 00:34:38 -0400

    modified more files

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,21 @@
 Rails.application.routes.draw do
   root 'apartments#index'
 
-
   devise_for :users
 
   resources :aboutdetails, only: [:index]
   resources :apply, only: [:index]
   resources :apartments do
-
-    # , only: [:create, :destroy, :edit, :index, :new, :show, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :photos
 
-    # resources :photos, only: [:create, :destroy, :edit, :index, :new, :show, :update]
-# get 'pages/about'
 end
-# resources :about
-# resources :contact
 
   namespace :api do
     namespace :v1 do
       resources :apartments
       resources :photos
       resources :users
-
-      # resources :photos do
-      #   ^was working earlier
-
-
-
-     # get 'photos/apartment_id', :to => "photos#show", :as => "apartment_id"
-
-
-     # end
-     # ^^was working working earlier
 
     end
   end
