ae02fbb18131dab31ef675d7f93b849240b55a9e
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-04-26 15:15:10 -0400

    reinstalled webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,11 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+console.log('Hello World from Webpacker')
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../react/containers/App';
+import App from '../react/containers/app';
 
 ReactDOM.render(
   <App />,
