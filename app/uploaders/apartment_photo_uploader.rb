b79712113f23e8e8e245e009219a9e1ad6c06876
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-05-03 02:49:24 -0400

    editing default carrierwave thumbnail pic

--- app/uploaders/apartment_photo_uploader.rb
+++ app/uploaders/apartment_photo_uploader.rb
@@ -9,9 +9,9 @@ class ApartmentPhotoUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
-  def serializable_hash
-     model.read_attribute :image
-   end
+  # def serializable_hash
+  #    model.read_attribute :image
+  #  end
 
 
   # Choose what kind of storage to use for this uploader:
@@ -30,15 +30,7 @@ class ApartmentPhotoUploader < CarrierWave::Uploader::Base
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-  def default_url
-    'blacklogo.jpg'
-  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
